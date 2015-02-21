2fbf5e76bc1e39577adf1e7c5c887a938fb5657d
denisHordienko <denisHordienko@a90d8e92-ce23-74ea-0765-251adc426f32>
2015-02-21 22:01:54 +0000

    Denis outpost render

--- trunk/Quantum/Quantum/Quantum/Renders/GlobalRender.cs
+++ trunk/Quantum/Quantum/Quantum/Renders/GlobalRender.cs
@@ -20,28 +20,49 @@ namespace Quantum.Quantum
         private Pen grayPen = new Pen(Color.Gray, 3);
         public void execute(GameEvent gameEvent)
         {
-            var general = gameEvent.model.currentGeneral;
+            General general = gameEvent.model.currentGeneral;
             Image generalImage;
 
-            if (general.Team == Team.green)
+            if (general.CurrentTeam == Team.green)
                 generalImage = greenGeneralImage;
             else
                 generalImage = blueGeneralImage;
 
             Matrix mat = new Matrix();
-            //Matrix mat = new Matrix(1, -1, 1, -1, 0, 0);
-
             double angle = Math.Atan2(general.Velocity.Y, general.Velocity.X);
-
             mat.RotateAt(90 + (float)(180 * angle / Math.PI), new PointF((float)general.Position.X, (float)general.Position.Y));
             mat.Translate(-30, -55);
-           // mat.Translate(10f, 10f);
-
-            gameEvent.graphics.Transform = mat;
-
+            gameEvent.graphics.Transform = mat;           
             gameEvent.graphics.DrawImage(generalImage, new Point((int)general.Position.X, (int)general.Position.Y));
-
             gameEvent.graphics.Transform = new Matrix();
+        }
+        public void drawOutposts(GameEvent gameEvent) 
+        {
+            List<Outpost> Outposts = gameEvent.model.Outposts;
+            foreach (Outpost outpost in Outposts) 
+            {
+                System.Drawing.Point centerOfOutpost = new System.Drawing.Point( (int)(-outpost.Position.X/2), (int)(-outpost.Position.Y/2));
+                if (outpost.id == 1)
+                    gameEvent.graphics.DrawImage(greyOutpostImage, centerOfOutpost);
+                if (outpost.id == 2)
+                    gameEvent.graphics.DrawImage(blueOutpostImage, centerOfOutpost);
+                if (outpost.id == 3)
+                    gameEvent.graphics.DrawImage(greenOutpostImage, centerOfOutpost);
+            }
+        }
+        public void drawDrone(GameEvent gameEvent) 
+        {
+            //Drone drone = gameEvent.model.
+        }
+        private void SimpleRotation(Image image, General general)
+        {
+            
+        }
+        private void RotateImage(Image image, General general)
+        {
+            Graphics graph = Graphics.FromImage(image);
+            double rotateAngel = Math.Atan2(general.Velocity.Y, general.Velocity.X) * (180 / Math.PI);
+            graph.RotateTransform((float)rotateAngel);     
             //gameEvent.graphics.DrawEllipse(grayPen, general.Position.X - 10, general.Position.Y - 10, 20, 20);
 
         }
