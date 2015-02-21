0838d36c3fe548960061384d42c37f82638225b7
amid.ukr <amid.ukr@a90d8e92-ce23-74ea-0765-251adc426f32>
2015-02-21 23:57:25 +0000

    Outpost conquer model

---
+++ Quantum/Quantum/Quantum/Controllers/OutpostConquestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quantum.Quantum.Controllers
+{
+    class OutpostConquestController : GameController
+    {
+        double usedTime;
+
+        public void execute(GameEvent gameEvent)
+        {
+            QuantumModel model = gameEvent.model;
+            usedTime += gameEvent.deltaTime;
+
+            if (usedTime < model.outpostConquestTime)
+            {
+                return;
+            }
+
+            Console.WriteLine("Check outposts");
+
+            usedTime -= model.outpostConquestTime;
+
+            
+
+            foreach(Outpost outpost in gameEvent.model.Outposts) {
+                List<General> generalsParticipators = new List<General>();
+
+                foreach(General general in model.Generals) {
+                    if(general.FindDroneCloseToOutpost(outpost, model.cloudRadius) != null) {
+                        generalsParticipators.Add(general);
+                        break;
+                    }
+                }
+
+                if (generalsParticipators.Count == 1)
+                {
+                    outpost.Team = generalsParticipators[0].Team;
+                }
+            }
+        }
+    }
+}
